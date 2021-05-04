e426ff661d122ae5a90b10b8a87d02b0d3a8b034
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-05-04 14:58:25 +0200

    added style for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,24 @@
+/* This component will be responsible for rendering different headers */
 import React from 'react';
+import ReactDOM from 'react-dom';
+import GuestHeader from '../../headers/GuestHeader/GuestHeader';
+import UserHeader from '../../headers/UserHeader/UserHeader';
 import './Header.css';
 
 const Header = (props) => {
 
-    return (
-        <header>
-            <h1>Our React Header. Probably will be on every page </h1>
-        </header>
-    );
+        const isLoggedIn = props.isLoggedIn;
+        if (isLoggedIn) {
+          return < UserHeader />;
+        }
+        else if(!isLoggedIn){ return <GuestHeader />;}
+    
 };
+
+ReactDOM.render(
+  //comment this line when you integrate
+   <Header isLoggedIn={true} />,
+   document.getElementById('root')
+  );
 
 export default Header;
