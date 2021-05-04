f6b1aff92429dbf7746537bdc8d7a1d350c391d8
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-05-04 12:42:36 +0200

    added burger menu component for smaller browser windows

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Menuu from './components/Menuu/Menuu';
 
 function App() {
+  
   return (
     <div className="App">
       <Header />
-      <Main />
-    </div>
+      <Menuu />
+      <Main/>
+          </div>
   );
 }
 
