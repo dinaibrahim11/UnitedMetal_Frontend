c9aa431fb9b20ece37bf46541303cc794e58b57b
dinaibrahim11 <81977280+dinaibrahim11@users.noreply.github.com>
2021-05-05 11:33:53 +0200

    Update Menuu.js
    fixed isloggedin issue

--- src/components/Menuu/Menuu.js
+++ src/components/Menuu/Menuu.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Menuu.css';
-import {useState, useContext } from "react"
 import GuestMenu from '../../menues/GuestMenu/GuestMenu';
 import UserMenu from '../../menues/UserMenu/UserMenu';
-import {isLoggedInContext} from '../../App'
+import { useSelector } from 'react-redux';
 const Menuu = (props) => {
-  const isloggedin = useContext(isLoggedInContext)
-    if (isloggedin) {
-      return <UserMenu />;
-    }
-  else { return <GuestMenu />;}
+  
 
+    //let renderedHeader = ;
+
+
+    return (
+        <div>
+        {isLoggedIn ? <UserMenu /> : <GuestMenu />}
+        </div>
+    );
 };
 
 export default Menuu;
