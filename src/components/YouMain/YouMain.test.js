2b022a5e229e6da1894fd31dfeb896c49da94082
amamdouhmahfouz <abdelrahman.abdelghany99@eng-st.cu.edu.eg>
2021-05-08 18:02:18 +0200

    used Provider in YouMaintest

--- src/components/YouMain/YouMain.test.js
+++ src/components/YouMain/YouMain.test.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import YouMain from '../YouMain/YouMain'
 import { render } from '@testing-library/react'
+import { Provider } from 'react-redux';
+import store from '../../storev2/store';
 
 it ('Renders toolbar correctly', () => {
     const match = {params : { id: 1 } };
-    const {getByText} = render(<YouMain match={match} currentTab='about'/>)
+    const {getByText} = render(<Provider store={store}><YouMain match={match} currentTab='about'/></Provider>)
     expect(getByText('About')).toBeTruthy()
     expect(getByText('Photostream')).toBeTruthy()
     expect(getByText('Albums')).toBeTruthy()
