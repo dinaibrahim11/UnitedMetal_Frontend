79af9a3821fd0219033cbb2a85bdd81f62d926d4
amamdouhmahfouz <abdelrahman.abdelghany99@eng-st.cu.edu.eg>
2021-05-30 01:27:50 +0200

    edit and delete comment done, needs BE testing

---
+++ src/components/UI/ShareModal/ShareModal.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+
+const ShareModal = (props) => {
+
+    const [isLinkCopied, setIsLinkCopied] = useState(false);
+
+
+    return (
+        <Modal
+        show={props.isShareModalOpen}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        onHide={props.handleCloseShareModal}
+        
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                {props.modalTitle}
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <h6>
+                External Share Link
+                </h6>
+                <CopyToClipboard style={{float: 'right', marginTop: '60px', marginRight: '20px'}} text={props.externalShareLink} onCopy={props.handleCopyToClipboard}>
+                    <span>{isLinkCopied ? "Copied!" : <FileCopyOutlinedIcon />}</span>
+                </CopyToClipboard>
+                <input
+                    style={{fontSize: '15px', width: '70%', textAlign: 'center', display: 'block', margin: '70px auto', padding: '0 40px', height: '53px' }} 
+                    readonly="" type="text" class="grab-link-text-field" value={props.externalShareLink} id="yui_3_16_0_1_1618999453530_59005"></input>
+            </Modal.Body>
+
+        </Modal>
+    );
+}
+
+export default ShareModal;
