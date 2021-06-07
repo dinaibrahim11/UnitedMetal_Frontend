558d2e849dc4d0fbcbe9bc3e2ac6aa4b1c98bb48
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-06-07 21:51:26 +0200

    styled faves and photostream

--- src/pages/YouFaves/YouFaves.js
+++ src/pages/YouFaves/YouFaves.js
@@ -6,6 +6,11 @@ import API from '../../fakeAPI';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+
 
       /**
  * @function YouFaves
@@ -25,33 +30,43 @@ import GridListTile from '@material-ui/core/GridListTile';
             justifyContent: 'space-around',
             overflow: 'hidden',
             backgroundColor: theme.palette.background.paper,
-            
           },
           gridList: {
-            width: 900,
+            width: 500,
             height: 450,
+            // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+            transform: 'translateZ(0)',
+          },
+          titleBar: {
+            background:
+              'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+              'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+          },
+          icon: {
+            color: 'white',
           },
         }));
+   
        const YouFaves = (props) => {
-         
+        useEffect(() => {
+          API.get(`user/${props.userId}/faves`, { 
+              headers: {
+                  "authorization": `Bearer ${props.token}` 
+              }}).then(res => {
+              console.log("FAVOURITES");
+              console.log(res);
+              setPhotos(res.data.data.favourites);
+          }).catch(err => {
+              console.log(err.response);
+          });
+  
+      }, []);
+  
         const classes = useStyles();
         const history = useHistory();
         const [photos, setPhotos] = useState([]);
     
-        useEffect(() => {
-            API.get(`user/${props.userId}/faves`, { 
-                headers: {
-                    "authorization": `Bearer ${props.token}` 
-                }}).then(res => {
-                console.log("FAVOURITES");
-                console.log(res);
-                setPhotos(res.data.data.favourites);
-            }).catch(err => {
-                console.log(err.response);
-            });
-    
-        }, []);
-    
+     
         const goToImage = (photoId) => {
             history.push(`/photos/${photoId}`);
         }
@@ -59,14 +74,30 @@ import GridListTile from '@material-ui/core/GridListTile';
 
            return (
             <div className={classes.root}>
-            <GridList cellHeight={160} className={classes.gridList} cols={4} >
+            <GridList cellHeight={240} spacing={2}  className={classes.gridList} cols={2} >
+            
               {photos.map((tile) => (
-                <GridListTile key={tile._id} cols={tile.cols || 1} onClick={() => goToImage(tile._id)}>
+                <GridListTile key={tile._id}  cols={tile.cols ? 2 : 1} rows={tile.cols ? 2 : 1} onClick={() => goToImage(tile._id)}>
                   <img src={tile.sizes.size.original.source} alt={tile.title} />
+                
+                   <GridListTileBar
+                    title={tile.title}
+                    titlePosition="buttom"
+                    subtitle={<span>by {tile.author}</span>}
+                    actionIcon={
+                      <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
+                        <StarBorderIcon />
+                      </IconButton>
+                    }
+                    actionPosition="right"
+                    className={classes.titleBar}
+                  />
                 </GridListTile>
               ))}
             </GridList>
           </div>
+      
+            
              
      /* <div>
       <title> Favorites | Flickr</title>
--- src/pages/YouPhotostream/YouPhotostream.js
+++ src/pages/YouPhotostream/YouPhotostream.js
@@ -6,9 +6,11 @@ import API from '../../fakeAPI';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
-
-      
 /**
  * @function YouPhotostream
  * @author Dina Mohsen
@@ -27,34 +29,44 @@ import GridListTile from '@material-ui/core/GridListTile';
     justifyContent: 'space-around',
     overflow: 'hidden',
     backgroundColor: theme.palette.background.paper,
-    
   },
   gridList: {
-    width: 900,
+    width: 500,
     height: 450,
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  icon: {
+    color: 'white',
   },
 }));
 
 const YouPhotostream = (props) => {
 
+  useEffect(() => {
+    API.get(`user/${props.userId}/stream`, { 
+        headers: {
+            "authorization": `Bearer ${props.token}` 
+        }}).then(res => {
+        console.log("PHOTOSTREAM");
+        console.log(res);
+        setPhotos(res.data.data.photos);
+    }).catch(err => {
+        console.log(err.response);
+    });
+
+}, []);
+
   const classes = useStyles();
     const history = useHistory();
     const [photos, setPhotos] = useState([]);
 
-    useEffect(() => {
-        API.get(`user/${props.userId}/stream`, { 
-            headers: {
-                "authorization": `Bearer ${props.token}` 
-            }}).then(res => {
-            console.log("PHOTOSTREAM");
-            console.log(res);
-            setPhotos(res.data.data.photos);
-        }).catch(err => {
-            console.log(err.response);
-        });
-
-    }, []);
-
+   
     const goToImage = (photoId) => {
         history.push(`/photos/${photoId}`);
     }
@@ -64,10 +76,24 @@ const YouPhotostream = (props) => {
         
     return (
       <div className={classes.root}>
-      <GridList cellHeight={160} className={classes.gridList} cols={4} >
+      <GridList cellHeight={240} spacing={2}  className={classes.gridList} cols={2} >
+      
         {photos.map((tile) => (
-          <GridListTile key={tile._id} cols={tile.cols || 1} onClick={() => goToImage(tile._id)}>
+          <GridListTile key={tile._id}  cols={tile.cols ? 2 : 1} rows={tile.cols ? 2 : 1} onClick={() => goToImage(tile._id)}>
             <img src={tile.sizes.size.original.source} alt={tile.title} />
+          
+             <GridListTileBar
+              title={tile.title}
+              titlePosition="buttom"
+              subtitle={<span>by {tile.author}</span>}
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="right"
+              className={classes.titleBar}
+            />
           </GridListTile>
         ))}
       </GridList>
