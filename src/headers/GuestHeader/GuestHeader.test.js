5c66b41b789239885d3f3ac2442ecb669f150101
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-06-08 18:22:56 +0200

    added unit testing for headers

---
+++ src/headers/GuestHeader/GuestHeader.test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import GuestHeader from './GuestHeader'
+import '@testing-library/jest-dom/extend-expect'
+
+describe('Input value', () => {
+    it('updates on change', () => {
+      const setSearchQuery = jest.fn((value) => {})
+      
+      const { queryByPlaceholderText } = render(<GuestHeader setSearchQuery={setSearchQuery}/>)
+  
+      const searchInput = queryByPlaceholderText('Photos, people, or groups')
+  
+      fireEvent.change(searchInput, { target: { value: 'test' } })
+  
+      expect(searchInput.value).toBe('test')
+    })
+  })
