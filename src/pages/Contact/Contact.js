08a8db4e7a1baa8701fd17390b6c2944c49ea580
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-10-04 22:09:53 +0200

    Adding contact page

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,20 @@
+
+ import React from 'react'
+ import {Link} from 'react-router-dom'
+ /**
+ * Page contact
+ * @function Contact
+ * @author Dina Mohsen
+ */
+
+ 
+ const Contact = (props) => {
+     return(
+ <div></div>
+ 
+     )
+ }
+ 
+ 
+ 
+ export default Contact;
