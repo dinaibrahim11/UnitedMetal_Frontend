86479dbe0b60f1c3b9bbc642d5038680521caf23
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-10-04 22:10:24 +0200

    Adding projects page

---
+++ src/pages/Projects/Projects.js
@@ -0,0 +1,20 @@
+
+ import React from 'react'
+ import {Link} from 'react-router-dom'
+ /**
+ * Page projects
+ * @function Projects
+ * @author Dina Mohsen
+ */
+
+ 
+ const Projects = (props) => {
+     return(
+ <div></div>
+ 
+     )
+ }
+ 
+ 
+ 
+ export default Projects;
