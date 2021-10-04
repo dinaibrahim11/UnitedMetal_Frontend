7a374916f37263b2dd4ff3af26e50bcf79bc0cf1
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-10-05 01:04:10 +0200

    Carrers page new features

--- src/pages/Careers/Careers.js
+++ src/pages/Careers/Careers.js
@@ -10,7 +10,176 @@
  
  const Careers = (props) => {
      return(
- <div></div>
+ <div>
+ <base href="https://braes.co/" />
+ <title>About Us - BRÆS</title>
+ <meta name="keywords" content="About Us" />
+ <meta name="description" content="About Us" />
+ <meta charSet="utf-8" />
+ <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+ <meta name="robots" content="max-image-preview:large" />
+ <link rel="alternate" hrefLang="ar-eg" href="https://braes.co//index/?action=lang&Id=Arabic&return=__2Faboutus__2F" />
+ <link rel="icon" href="themes/braes/assets/img/cropped-4E8229D4-1B5F-4447-B899-BD67042E4819-32x32.png" sizes="32x32" />
+ <link rel="icon" href="themes/braes/assets/img/cropped-4E8229D4-1B5F-4447-B899-BD67042E4819-192x192.png" sizes="192x192" />
+ <link rel="apple-touch-icon" href="themes/braes/assets/img/cropped-4E8229D4-1B5F-4447-B899-BD67042E4819-180x180.png" />
+ <meta name="msapplication-TileImage" content="themes/braes/assets/img/cropped-4E8229D4-1B5F-4447-B899-BD67042E4819-270x270.png" />
+ <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap" rel="stylesheet" />
+ <link href="themes/braes/assets/css/bootstrap.custom.min.css" rel="stylesheet" />
+ <link href="themes/braes/assets/css/style.css?key=54" rel="stylesheet" />
+ <link href="themes/braes/assets/css/custom.css" rel="stylesheet" />
+ <style type="text/css" dangerouslySetInnerHTML={{__html: "\n        .mt-3 a {\n            color: #bf8c8a !important;\n            text-decoration: none;\n            background-color: transparent;\n        }\n    " }} />
+ <noscript>&lt;img height="1" width="1" style="display:none"
+   src="https://www.facebook.com/tr?id=4070651586295301&amp;ev=PageView&amp;noscript=1" /&gt;</noscript>
+ <noscript>&lt;img height="1" width="1" style="display:none"
+   src="https://www.facebook.com/tr?id=433856431319815&amp;ev=PageView&amp;noscript=1" /&gt;</noscript>
+ <div id="page">
+ 
+   <style type="text/css" dangerouslySetInnerHTML={{__html: "\n    @media (max-width: 767px) {\n\n        .qodef-testimonials-mark {\n            left: unset;\n\n        }\n    }\n\n    .box_about p {\n        color: #000 !important;\n    }\n\n    .card-header:first-child {\n        border-radius: calc(.25rem - 1px) calc(.25rem - 1px) 0 0;\n    }\n\n    .card-header {\n        padding: .75rem 1.25rem;\n        margin-bottom: 0;\n        background-color: rgba(0, 0, 0, .03);\n        border-bottom: 1px solid rgba(0, 0, 0, .125);\n    }\n\n    .btn-link {\n        font-weight: 400;\n        color: #007bff;\n        background-color: transparent;\n    }\n\n    .btn {\n        display: inline-block;\n        font-weight: 400;\n        text-align: center;\n        white-space: nowrap;\n        vertical-align: middle;\n        -webkit-user-select: none;\n        -moz-user-select: none;\n        -ms-user-select: none;\n        user-select: none;\n        border: 1px solid transparent;\n        padding: .375rem .75rem;\n        font-size: 1rem;\n        line-height: 1.5;\n        border-radius: .25rem;\n        transition: color .15s ease-in-out, background-color .15s ease-in-out, border-color .15s ease-in-out, box-shadow .15s ease-in-out;\n    }\n    .about-img{text-align: center; width: 100%;}\n    .about-text {text-align: center;\n\twidth: 100%; margin-bottom: 10px;\n}\n.about-text p{margin: 5px }\n/*\n@media (max-width: 1024px) {\n    .about-img{float: none; margin: 0 auto;}\n    .about-text {float: none; width: 100%; padding: 5px 7px 10px 29px;}\n    .about-text p{margin: 0 auto !important;display: block;}\n}*/\n" }} />
+   <link href="themes/braes/assets/css/about.css?key=1" rel="stylesheet" />
+   <main className="bg_gray">
+     <div className="container ">
+      
+       <div className="container" style={{position: 'relative', paddingTop: '20px', marginBottom: '25px', display: 'none'}}>
+         <div className="qodef-testimonials-mark">”</div>
+       </div>
+       <div className="container ">
+         <div className="row justify-content-center align-items-center">
+           <div className="col-lg-6">
+             <div className="box_about">
+               <h2> Great jobs
+Are you ready for a special job?
+ </h2>
+               <p>Find job details and your point of contact with only one click. 
+                You can send us an e-mail to apply for the job and you will recieve a reply as soon as your application has been sent successfully.
+</p>
+             </div>
+           </div>
+           <div className="col-md-6 col-lg-6 col-12 col-sm-12">
+             <div className="prod_info">
+               <h1> .</h1>
+             </div>
+             <div id="accordion">
+
+               <div className="card">
+                 <div className="card-header" id="headingOne">
+                   <h5 className="mb-0">
+                     <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                     Regional manager
+                     </button>
+                   </h5>
+                 </div>
+                 <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                   <div className="card-body">
+                   Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+
+Nunc dignissim risus id metus.
+Cras ornare tristique elit.
+Vivamus vestibulum ntulla nec ante.
+Praesent placerat risus quis eros.
+Fusce pellentesque suscipit nibh.
+Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+                   </div>
+                 </div>
+               </div>
+               
+               <div className="card">
+                 <div className="card-header" id="headingTwo">
+                   <h5 className="mb-0">
+                     <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                     IT specialist
+                     </button>
+                   </h5>
+                 </div>
+                 <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                   <div className="card-body">
+                     <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+
+Nunc dignissim risus id metus.
+Cras ornare tristique elit.
+Vivamus vestibulum ntulla nec ante.
+Praesent placerat risus quis eros.
+Fusce pellentesque suscipit nibh.
+Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                   </div>
+                 </div>
+               </div>
+               <div className="card">
+                 <div className="card-header" id="headingTwo">
+                   <h5 className="mb-0">
+                     <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                     Sales consultant 
+                     </button>
+                   </h5>
+                 </div>
+                 <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                   <div className="card-body">
+                     <p>Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+
+Nunc dignissim risus id metus.
+Cras ornare tristique elit.
+Vivamus vestibulum ntulla nec ante.
+Praesent placerat risus quis eros.
+Fusce pellentesque suscipit nibh.
+Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                   </div>
+                 </div>
+               </div>
+             
+               <div className="card">
+                 <div className="card-header" id="headingThree">
+                   <h5 className="mb-0">
+                     <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                     HR manager
+                     </button>
+                   </h5>
+                 </div>
+                 <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                   <div className="card-body">
+                   Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+
+Nunc dignissim risus id metus.
+Cras ornare tristique elit.
+Vivamus vestibulum ntulla nec ante.
+Praesent placerat risus quis eros.
+Fusce pellentesque suscipit nibh.
+Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+                   </div>
+                 </div>
+               </div>
+               <div className="card">
+                 <div className="card-header" id="headingThree">
+                   <h5 className="mb-0">
+                     <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                     Java developer 
+                     </button>
+                   </h5>
+                 </div>
+                 <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                   <div className="card-body">
+                   Leverage agile frameworks to provide a robust synopsis for high level overviews. Iterative approaches to corporate strategy foster collaborative thinking to further the overall value proposition. Organically grow the holistic world view of disruptive innovation via workplace diversity and empowerment.
+
+Nunc dignissim risus id metus.
+Cras ornare tristique elit.
+Vivamus vestibulum ntulla nec ante.
+Praesent placerat risus quis eros.
+Fusce pellentesque suscipit nibh.
+Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.
+                   </div>
+                 </div>
+               </div>
+             
+             </div>
+           </div>
+         </div>
+         <br />
+       </div>
+     </div></main>
+   <div className="fb-customerchat" page_id={420189051856950}>
+   </div>
+ </div>
+ <div id="toTop" />
+</div>
  
      )
  }
