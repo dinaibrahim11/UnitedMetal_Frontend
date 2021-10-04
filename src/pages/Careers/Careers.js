42741f6b8097b572c58383b57fb76c95185a8055
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-10-04 22:09:38 +0200

    Adding careers page

---
+++ src/pages/Careers/Careers.js
@@ -0,0 +1,20 @@
+
+ import React from 'react'
+ import {Link} from 'react-router-dom'
+ /**
+ * Page careers
+ * @function Careers
+ * @author Dina Mohsen
+ */
+
+ 
+ const Careers = (props) => {
+     return(
+ <div></div>
+ 
+     )
+ }
+ 
+ 
+ 
+ export default Careers;
