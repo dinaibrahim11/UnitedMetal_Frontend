ea1787e761a0a375ea269dbba075330b1b16a47e
Dina Ibrahim <dina.ibrahim.1@outlook.com>
2021-10-04 22:10:43 +0200

    Adding services page

---
+++ src/pages/Services/Services.js
@@ -0,0 +1,20 @@
+
+ import React from 'react'
+ import {Link} from 'react-router-dom'
+ /**
+ * Page services
+ * @function Services
+ * @author Dina Mohsen
+ */
+
+ 
+ const Services = (props) => {
+     return(
+ <div></div>
+ 
+     )
+ }
+ 
+ 
+ 
+ export default Services;
